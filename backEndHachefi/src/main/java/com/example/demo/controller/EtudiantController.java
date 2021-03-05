59117adff64da7889325fa8fe86725a4ed4c4c3a
hachefi <fati.hachefi@gmail.com>
2021-03-05 15:31:23 +0100

    added count number of students.

--- backEndHachefi/src/main/java/com/example/demo/controller/EtudiantController.java
+++ backEndHachefi/src/main/java/com/example/demo/controller/EtudiantController.java
@@ -3,15 +3,17 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Etudiant;
-import com.example.demo.repository.AbsenceRepository;
 import com.example.demo.repository.EtudiantRepository;
 
 @RestController
@@ -34,6 +36,10 @@ public class EtudiantController {
 	@DeleteMapping("/delete/{id}")
 	public void deleteEtudiant(@PathVariable Integer id) {
 		etudiantRepo.deleteById(id);
+	}
+	@GetMapping("/count")
+	public Long countEtudiant() {
+		return etudiantRepo.count();
 	}
 	@PutMapping("/update/{id}")
 	public Etudiant updateEtudiant(@RequestBody Etudiant etd , @PathVariable Integer id ) {
